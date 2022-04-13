7f4fab8df56ae5a0bf58bfae63683cb3808e7a0c
Nikitty <100781355+Nikitty@users.noreply.github.com>
2022-04-13 10:46:43 +0200

    main

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -1,15 +1,38 @@
 sap.ui.define(
-  ['sap/ui/core/mvc/Controller'],
+  ['sap/ui/core/mvc/Controller', 'sap/m/Button', 'sap/m/Dialog', 'sap/m/Table'],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller) {
+  function (Controller, Button, Dialog, Table) {
     'use strict'
 
     return Controller.extend('project1.controller.View1', {
       onInit: function () {},
       onClick: function () {
-        MessageToast.show('Button is clicked')
+        if (!this.oDialog) {
+          this.oDialog = new Dialog({
+            title: 'Table',
+            // content: ,
+
+            beginButton: new Button({
+              text: 'OK',
+              press: function () {
+                this.oDialog.close()
+              }.bind(this),
+            }),
+
+            endButton: new Button({
+              text: 'Close',
+              press: function () {
+                this.oDialog.close()
+              }.bind(this),
+            }),
+          })
+
+          this.getView().addDependent(this.oDialog)
+        }
+
+        this.oDialog.open()
       },
     })
   }
