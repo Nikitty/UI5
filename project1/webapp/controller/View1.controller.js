435b1c858b74c797797765a9f453184583896313
Nikitty <100781355+Nikitty@users.noreply.github.com>
2022-04-13 15:02:06 +0200

    main

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -1,9 +1,25 @@
 sap.ui.define(
-  ['sap/ui/core/mvc/Controller', 'sap/m/Button', 'sap/m/Dialog', 'sap/m/Table'],
+  [
+    'sap/ui/core/mvc/Controller',
+    // 'sap/m/MessageToast',
+    'sap/m/Button',
+    'sap/m/Dialog',
+    // 'sap/m/List',
+    // 'sap/m/StandardListItem',
+    'sap/m/Table',
+  ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, Button, Dialog, Table) {
+  function (
+    Controller,
+    // MessageToast,
+    Button,
+    Dialog,
+    // List,
+    // StandardListItem,
+    Table
+  ) {
     'use strict'
 
     return Controller.extend('project1.controller.View1', {
@@ -12,7 +28,47 @@ sap.ui.define(
         if (!this.oDialog) {
           this.oDialog = new Dialog({
             title: 'Table',
-            // content: ,
+            content: new Table({
+              id: 'mainViewTbl',
+              columns: [
+                new sap.m.Column({
+                  header: [new sap.m.Label({ text: '{i18n>workOrderNo}' })],
+                }),
+                new sap.m.Column({
+                  header: [new sap.m.Label({ text: '{i18n>orderNo}' })],
+                }),
+                new sap.m.Column({
+                  header: [new sap.m.Label({ text: '{i18n>location}' })],
+                }),
+                new sap.m.Column({
+                  header: [new sap.m.Label({ text: '{i18n>lineEnterDate}' })],
+                }),
+                new sap.m.Column({
+                  header: [new sap.m.Label({ text: '{i18n>operation}' })],
+                }),
+              ],
+
+              items: [
+                new sap.m.ColumnListItem({
+                  cells: [
+                    new sap.m.Text({ text: '1' }),
+                    new sap.m.Text({ text: '2' }),
+                    new sap.m.Text({ text: '3' }),
+                    new sap.m.Text({ text: '4' }),
+                    new sap.m.Text({ text: '5' }),
+                  ],
+                }),
+                new sap.m.ColumnListItem({
+                  cells: [
+                    new sap.m.Text({ text: '6' }),
+                    new sap.m.Text({ text: '7' }),
+                    new sap.m.Text({ text: '8' }),
+                    new sap.m.Text({ text: '9' }),
+                    new sap.m.Text({ text: '10' }),
+                  ],
+                }),
+              ],
+            }),
 
             beginButton: new Button({
               text: 'OK',
